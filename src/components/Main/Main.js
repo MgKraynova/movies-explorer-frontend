952867302b054fe25fcbebc37c72fa5f9dd92eaa
Marina Kraynova <kraynovamg@gmail.com>
2022-05-05 16:16:18 +0300

    add adaptive to student info section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -81,51 +81,51 @@ function Main() {
                         </ul>
                     </div>
                 </section>
-                {/*<section className="student-info section">*/}
-                {/*    <div className="section__header">*/}
-                {/*        <h2 className="section__title">Студент</h2>*/}
-                {/*    </div>*/}
-                {/*    <div className="student-info__content">*/}
-                {/*        <div className="student-info__text-container">*/}
-                {/*            <div className="student-info__main-info">*/}
-                {/*                <div className="student-info__wrapper">*/}
-                {/*                    <h3 className="student-info__subtitle">Марина</h3>*/}
-                {/*                    <p className="student-info__caption">Фронтенд-разработчик, 26 лет</p>*/}
-                {/*                </div>*/}
-                {/*                <p className="student-info__description">Я родился и живу в Саратове, закончил факультет экономики СГУ.*/}
-                {/*                    У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.*/}
-                {/*                    Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того,*/}
-                {/*                    как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>*/}
-                {/*            </div>*/}
-                {/*            <ul className="student-info__list list">*/}
-                {/*                <li>*/}
-                {/*                    <a href="https://ru-ru.facebook.com/" className="student-info__link link">Facebook</a>*/}
-                {/*                </li>*/}
-                {/*                <li>*/}
-                {/*                    <a href="https://github.com/MgKraynova" className="student-info__link link">Github</a>*/}
-                {/*                </li>*/}
-                {/*            </ul>*/}
-                {/*        </div>*/}
-                {/*        <img className="student-info__img" src={photo} alt="Фото Марины"/>*/}
-                {/*    </div>*/}
-                {/*</section>*/}
-                {/*<section className="portfolio section" >*/}
-                {/*    <h2 className="portfolio__title">Портфолио</h2>*/}
-                {/*    <ul className="portfolio__list list">*/}
-                {/*        <li className="portfolio__item">*/}
-                {/*            <a className="portfolio__link link" href="https://github.com/MgKraynova/how-to-learn">Статичный сайт</a>*/}
-                {/*            <img className="portfolio__icon" src={portfolioIcon} alt="Изображение стрелки" />*/}
-                {/*        </li>*/}
-                {/*        <li className="portfolio__item">*/}
-                {/*            <a className="portfolio__link link" href="https://github.com/MgKraynova/russian-travel">Адаптивный сайт</a>*/}
-                {/*            <img className="portfolio__icon" src={portfolioIcon} alt="Изображение стрелки" />*/}
-                {/*        </li>*/}
-                {/*        <li className="portfolio__item">*/}
-                {/*            <a className="portfolio__link link" href="https://github.com/MgKraynova/react-mesto-api-full">Одностраничное приложение</a>*/}
-                {/*            <img className="portfolio__icon" src={portfolioIcon} alt="Изображение стрелки" />*/}
-                {/*        </li>*/}
-                {/*    </ul>*/}
-                {/*</section>*/}
+                <section className="student-info section">
+                    <div className="section__header">
+                        <h2 className="section__title">Студент</h2>
+                    </div>
+                    <div className="student-info__content">
+                        <div className="student-info__text-container">
+                            <div className="student-info__main-info">
+                                <div className="student-info__wrapper">
+                                    <h3 className="student-info__subtitle">Марина</h3>
+                                    <p className="student-info__caption">Фронтенд-разработчик, 26 лет</p>
+                                </div>
+                                <p className="student-info__description">Я родился и живу в Саратове, закончил факультет экономики СГУ.
+                                    У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.
+                                    Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того,
+                                    как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                            </div>
+                            <ul className="student-info__list list">
+                                <li>
+                                    <a href="https://ru-ru.facebook.com/" className="student-info__link link">Facebook</a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/MgKraynova" className="student-info__link link">Github</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <img className="student-info__img" src={photo} alt="Фото Марины"/>
+                    </div>
+                </section>
+                <section className="portfolio section" >
+                    <h2 className="portfolio__title">Портфолио</h2>
+                    <ul className="portfolio__list list">
+                        <li className="portfolio__item">
+                            <a className="portfolio__link link" href="https://github.com/MgKraynova/how-to-learn">Статичный сайт</a>
+                            <img className="portfolio__icon" src={portfolioIcon} alt="Изображение стрелки" />
+                        </li>
+                        <li className="portfolio__item">
+                            <a className="portfolio__link link" href="https://github.com/MgKraynova/russian-travel">Адаптивный сайт</a>
+                            <img className="portfolio__icon" src={portfolioIcon} alt="Изображение стрелки" />
+                        </li>
+                        <li className="portfolio__item">
+                            <a className="portfolio__link link" href="https://github.com/MgKraynova/react-mesto-api-full">Одностраничное приложение</a>
+                            <img className="portfolio__icon" src={portfolioIcon} alt="Изображение стрелки" />
+                        </li>
+                    </ul>
+                </section>
             </main>
         </>
     )
