c84c2d81998d86bf951b94ec08e4f460adceebab
Marina Kraynova <kraynovamg@gmail.com>
2022-05-06 15:38:08 +0300

    add notFound component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import {Route, Routes} from 'react-router-dom';
 import Main from "../Main/Main";
 import Register from '../Register/Register';
-import Footer from "../Footer/Footer";
 import Login from "../Login/Login";
+import NotFound from "../NotFound/NotFound";
 
 function App() {
     return (
@@ -12,7 +12,7 @@ function App() {
                 <Route path="/signin" element={<Login />} />
                 <Route path="/signup" element={<Register />} />
 
-                {/*<Route path="*" element={<NotFound />} />*/}
+                <Route path="*" element={<NotFound />} />
             </Routes>
             {/*<Footer />*/}
 
