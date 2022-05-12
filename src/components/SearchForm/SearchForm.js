6f1c3fef3cb4502ce8eff96fe194d735673236f7
Marina Kraynova <kraynovamg@gmail.com>
2022-05-12 14:22:15 +0300

    add custom validation to searchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,18 +1,43 @@
 import './search.css';
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+import {useState} from "react";
 
 function SearchForm() {
-    return(
+
+    const [inputValidity, setInputValidity] = useState(false);
+    const [isErrorShown, setIsErrorShown] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+
+    function handleInputChange(event) {
+        setInputValue(event.target.value);
+        setIsErrorShown(false);
+        setInputValidity(event.target.validity.valid);
+    }
+
+    function handleSubmitForm(event) {
+        event.preventDefault();
+
+        if (!inputValidity || inputValue.length === 0) {
+            setIsErrorShown(true);
+        } else {
+            setIsErrorShown(false);
+        }
+    }
+
+    return (
         <div className="search">
-            <form className="search__form">
+            <form className="search__form" onSubmit={handleSubmitForm} noValidate={true}>
                 <input
                 className="search__input"
                 placeholder="Фильм"
-                required
+                // required
                 type="text"
-                minLength="1"
+                // minLength="1"
+                // value={movieInputValue}
+                onChange={handleInputChange}
                 />
                 <button className="search__button" type="submit">Найти</button>
+                <span className="search__error">{isErrorShown && 'Нужно ввести ключевое слово'}</span>
             </form>
             <div className="search__container">
                 <FilterCheckbox />
