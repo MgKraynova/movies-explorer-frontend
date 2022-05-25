98a0747f53f3f5227abd904f7810254ee96a6c48
Marina Kraynova <kraynovamg@gmail.com>
2022-05-25 09:49:32 +0300

    fix navigate at protectedRoute to /

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -3,7 +3,7 @@ import {Navigate} from "react-router-dom";
 function ProtectedRoute(props) {
     if (!localStorage.getItem('token')) {
         return (
-            <Navigate to="/signin" />
+            <Navigate to="/" />
         );
     }
 
